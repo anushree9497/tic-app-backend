48140cc432b2625192b45f1d67a4c1d17afcf91e
anushree.uppoor <anushree.uppoor@users.github.com>
2023-06-25 22:58:13 +0530

    first commit

---
+++ src/main/java/com/app/tic/TicApplication.java
@@ -0,0 +1,13 @@
+package com.app.tic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/tic/TicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.tic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
